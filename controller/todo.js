2feb6f7a0b4070b78a203b54f8823b522b81d794
Tamannatanwa <tamanna22@navgurukul.org>
2026-05-10 18:48:01 +0530

    refactor: improve error handling and code structure in todo controller

--- controller/todo.js
+++ controller/todo.js
@@ -1,125 +1,125 @@
-const todo = require("../model/todo")
-const { prisma } = require("../config/psqlConn");
+const todo = require("../model/todo");
 
+const addTodo = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { userId } = req.user;
 
-const addTodo = async (req,res)=>{
-    try{
-        const {name} = req.body;
-       const { userId } = req.user
+    if (!name) {
+      console.log("name is requied");
 
-        console.log(req)
-
-        if(!name){
-
-            console.log("name is requied")
-
-            return res.status(400).json({
-                msg:"name is requied"
-            })
-        } 
-
-        const exist = await todo.findOne({name})
-
-        if (exist){
-
-            console.log("User Already exists")
-
-            return res.status(400).json({
-                msg:"Already exists"
-            })
-        }
-
-        const addTAsk = await todo.create({name, user_id:userId})
-
-            console.log("Task Added" , addTAsk)
-
-        return res.status(201).json({
-            msg:"Todo Task Created Successfully !",
-            data:addTAsk
-        })
-
+      return res.status(400).json({
+        msg: "name is requied",
+      });
     }
-    catch(err){
-        console.error("Error:", err);
-        throw err
+
+    const exist = await todo.findOne({ name });
+
+    if (exist) {
+      console.log("User Already exists");
+
+      return res.status(400).json({
+        msg: "Already exists",
+      });
     }
-}
 
+    const addTAsk = await todo.create({ name, user_id: userId });
 
+    console.log("Task Added", addTAsk);
 
+    return res.status(201).json({
+      msg: "Todo Task Created Successfully !",
+      data: addTAsk,
+    });
+  } catch (err) {
+    console.error("Error:", err);
+    throw err;
+  }
+};
 
-const getAllTodo = async (req,res)=>{
-    try{
-        const { userId} = req.user
-        const allTodo = await todo.find({user_id:userId});
-        console.log("All Todo Tasks",allTodo)
-        return res.status(200).json({
-            msg:"all todo's",
-            data : allTodo
-        })
+const getAllTodo = async (req, res) => {
+  try {
+    const { userId } = req.user;
+    const allTodo = await todo.find({ user_id: userId });
+    console.log("All Todo Tasks", allTodo);
+    return res.status(200).json({
+      msg: "all todo's",
+      data: allTodo,
+    });
+  } catch (err) {
+    console.error("Error:", err);
+    throw err;
+  }
+};
+
+const getTodoById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.user;
+    const allTodo = await todo.findById({ _id: id });
+    if (!(allTodo?.user_id == userId)) {
+      return res.status(400).json({
+        msg: "Invalid UserId",
+      });
     }
-    catch(err){
-        console.error("Error:", err);
-        throw err
+    console.log("userData", allTodo);
+    return res.status(200).json({
+      msg: "todo task has been fetched",
+      data: allTodo,
+    });
+  } catch (err) {
+    console.error("Error:", err);
+    throw err;
+  }
+};
+
+const updateTodoById = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { id } = req.params;
+    const { userId } = req.user;
+    const allTodo = await todo.updateOne({ _id: id }, { $set: { name: name } });
+    if (!(allTodo?.user_id == userId)) {
+      return res.status(400).json({
+        msg: "Invalid UserId",
+      });
     }
-}
+    console.log("todo task has been updated", allTodo);
+    return res.status(200).json({
+      msg: "todo task has bee updated successfully !",
+      data: allTodo,
+    });
+  } catch (err) {
+    console.error("Error:", err);
+    throw err;
+  }
+};
 
-
-
-const getTodoById = async (req,res)=>{
-    try{
-        const {id} = req.params
-        const allTodo = await todo.findById({_id:id});
-        console.log("userData",allTodo)
-        return res.status(200).json({
-            msg:"todo task has been fetched",
-            data : allTodo
-        })
+const deleteTodoById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.user;
+    const allTodo = await todo.deleteOne({ _id: id });
+    if (!(allTodo?.user_id == userId)) {
+      return res.status(400).json({
+        msg: "Invalid UserId",
+      });
     }
-    catch(err){
-        console.error("Error:", err);
-        throw err 
-    }
-}
+    console.log("todo task deleted", allTodo);
+    return res.status(200).json({
+      msg: "todo task has bee deleted successfully !",
+      data: allTodo,
+    });
+  } catch (err) {
+    console.error("Error:", err);
+    throw err;
+  }
+};
 
-
-
-const updateTodoById = async (req,res)=>{
-    try{
-        const {name} = req.body;
-        const {id} = req.params
-        const allTodo = await todo.updateOne({_id:id},{$set:{name:name}});
-        console.log("todo task has been updated",allTodo)
-        return res.status(200).json({
-            msg:"todo task has bee updated successfully !",
-            data : allTodo
-        })
-
-    }
-    catch(err){
-        console.error("Error:", err);
-        throw err
-        
-    }
-}
-
-
-
-const deleteTodoById = async (req,res)=>{
-    try{
-        const {id} = req.params
-        const allTodo = await todo.deleteOne({_id:id});
-        console.log("todo task deleted",allTodo)
-        return res.status(200).json({
-            msg:"todo task has bee deleted successfully !",
-            data : allTodo
-        })
-    }
-    catch(err){
-        console.error("Error:", err);
-        throw err
-        
-    }
-}
-
-module.exports = {addTodo,getAllTodo ,getTodoById ,updateTodoById ,deleteTodoById}
+module.exports = {
+  addTodo,
+  getAllTodo,
+  getTodoById,
+  updateTodoById,
+  deleteTodoById,
+};
